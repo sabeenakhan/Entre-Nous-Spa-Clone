b3e74d44fcc731a845b6ce2a8f167cb1939f0cbf
Sabeenakhan <sabeena.khan94@gmail.com>
2025-03-08 22:05:22 +0530

    Adding Swiper Javasript

--- script.js
+++ script.js
@@ -67,3 +67,18 @@ function swapImages() {
 }
 
 swapImages()
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});

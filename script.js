2115d6b005a8d654c6a6a7c16293a5678278aa22
Sabeenakhan <sabeena.khan94@gmail.com>
2025-03-08 16:02:18 +0530

    Sidebar JavaScrict

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "68%";
+    // document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft= "0";
+}
